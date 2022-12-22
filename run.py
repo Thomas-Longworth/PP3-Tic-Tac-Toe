5d8cbaabfe348258ad318144d51cc2791100c376
Thomas-Longworth <thomaslongworth555@gmail.com>
2022-12-22 17:38:03 +0000

    added the game board

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+game_grid = [["_", "_", "_"], ["_", "_", "_"],["_", "_", "_"]]
+
+def game_board():
+    count = 0
+    print("    0    1    2")
+    for row in game_grid:
+        print(count, row)
+        count = count + 1
+
+game_board()
